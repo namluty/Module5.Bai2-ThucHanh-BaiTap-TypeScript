77e11552729b33a39c98bea51ba9b14f23bb4949
Your Name <dhnam.2020@gmail.com>
2021-10-15 17:02:21 +0700

    Bai 1 Thuc Hanh Bai Tap

---
+++ src/main.js
@@ -0,0 +1,4 @@
+var width = 18;
+var height = 21;
+var area = width * height;
+console.log("Di\u1EC7n t\u00EDch h\u00ECnh ch\u1EEF nh\u1EADt: " + area);
---
+++ src/main.ts
@@ -0,0 +1,4 @@
+let width: number =18;
+let height: number=21;
+let area: number = width*height;
+console.log(`Diện tích hình chữ nhật: ${area}`);
